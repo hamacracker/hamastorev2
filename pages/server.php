2e5387786d1b4f2099888e0e764ccefb4b42b9e4
HAMAEXPERT <77998844+hamacracker@users.noreply.github.com>
2023-04-07 08:05:11 +0300

    Add files via upload

---
+++ pages/server.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Server</title>
+</head>
+<body>
+  <div class="page">
+    <div class="navbar">
+      <div class="navbar-inner sliding">
+        <div class="left">
+ <a href="#" class="link back">
+          <i class="white-color f7-icons">arrow_left</i> Back  </a>
+        </div>
+        <div class="title">
+          Documentation Of Which Apps Are Available ?
+        </div>
+        <div class="right">
+          <a href="javascript:window.location.reload(true)" class=
+          "f7-icons">reload</a>
+        </div>
+      </div>
+    </div>
+    <div class="page-content">
+      <div class="list media-list inset elevation-12">
+        <ul>
+          <br>
+        <center>
+      Video Star âœ…
+          <br>
+     Video Star PRO ðŸš«
+          <br>
+      VideoFX Live ðŸš«
+          <br> 
+          Esign  âœ…
+          <br>
+          Unc0ver  âœ…
+          <br>
+          Odyssey âœ…
+          <br>
+          PPSSPP âœ…
+          <br>
+          ProCam Ã¢Å“â€¦
+          <br>
+          Bolt ðŸš«
+          <br>
+          TikTok LRD  âœ…
+          <br>
+          Pubg Mobile ✅
+          <br>
+          Gta : San Andreas ðŸš«
+          <br> 
+          Org : 2020 ðŸš«
+          <br>
+          8 Ball Pool ðŸš«
+          <br>
+          Minecraft âœ…
+          <br>
+          Kine Master  âœ…
+          <br>
+          AppCake Ã°Å¸Å¡
+          <br>
+          PicsArt âœ…
+          <br>
+          
+         </center>
+         </br>     
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
